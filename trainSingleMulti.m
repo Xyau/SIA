1d1780983482a2d0202fec236354f0bebb15bff4
nicomarcan <marcantonionicolas@gmail.com>
2018-03-14 19:40:23 -0300

    forgot

--- trainSingleMulti.m
+++ trainSingleMulti.m
@@ -3,8 +3,9 @@ function retval = trainSingleMulti (weights, desired,learning,activation,actDeri
   delta = actDerivative(intermediateValues{layers}) * (desired - intermediateValues{layers});
   weights{size(weights)(2)} = weights{size(weights)(2)} + learning*delta*addUmbral(intermediateValues{layers-1});
   for i = layers-1:-1:2
+    deltaOld = delta;
     for j = 1 : size(weights{i-1})(1)
-      delta = actDerivative(intermediateValues{i}(j)) * weights{i}(j+1) * delta' ; 
+      delta = actDerivative(intermediateValues{i}(j)) * weights{i}(j+1) * deltaOld' ; 
       weights{i-1}(j,:) = weights{i-1}(j,:) + learning * delta * addUmbral(intermediateValues{i-1});
     endfor
   endfor
