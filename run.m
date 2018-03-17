c004a1c317f094647d107e1f8f5fc2522bb1d563
nicomarcan <marcantonionicolas@gmail.com>
2018-03-17 17:11:54 -0300

    added pair

--- run.m
+++ run.m
@@ -7,18 +7,22 @@ function [] = run()
   f = @(x) x;
   f_prima = @(x) ones(size(x));
   xor = @(x) bitxor(bitxor(x(1),x(2)),x(3));
-  xor_activated = @(x) tanh(bitxor(bitxor(x(1),x(2)),x(3)));
+  xor_activated = @(x) f(bitxor(bitxor(x(1),x(2)),x(3)));
+  
+  pair = @(x) mod(x(1)+x(2)+x(3),2);
+  pair_activated = @(x) f(mod(x(1)+x(2)+x(3),2));
+  
   for eta = 0.05:0.05:1
     for k = 1:50
       for i = 1 : size(E)(1)
         [V,h] = sumaMultiActivation(a,E(i,:),f);
-        a = trainSingleMulti(a, xor_activated(E(i,:)),0.05,f,f_prima,V,h);
+        a = trainSingleMulti(a, pair_activated(E(i,:)),0.05,f,f_prima,V,h);
        endfor
     endfor
     correct =8;
     for i = 1 : size(E)(1)
         h = sumaMulti(a,E(i,:)); 
-        correct -= (h{size(V)(2)} > 0 -  xor(E(i,:)));
+        correct -= (h{size(V)(2)} > 0 -  pair(E(i,:)));
     endfor
     printf("eta %d -> %d\n",eta,correct * 100 / 8);
    endfor
