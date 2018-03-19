893a6973057d21c14944349d398399f61d45cfca
Eliseo Parodi Almaraz <eparodialmaraz@gmail.com>
2018-03-19 15:49:44 -0300

    Perceptron fixed and working ;)

--- Perceptron.m
+++ Perceptron.m
@@ -6,6 +6,8 @@ classdef Perceptron < handle
     network = {};
     activation;
     diffActivation;
+    costError = [];
+    costIndex = 0;
 
   end
 
@@ -13,53 +15,69 @@ classdef Perceptron < handle
 
   methods
 
-    function this = Perceptron(learningRate, sizes, numOfNeurons, activation, diffActivation)
+    function this = Perceptron(learningRate, network, activation, diffActivation)
       this.learningRate = learningRate;
       this.activation = activation;
       this.diffActivation = diffActivation;
-      for i = 1:size(sizes)(2)
-        this.network{i} = unifrnd(-1, 1, sizes(i), numOfNeurons(i));
-      end
+      this.network = network
     end
 
     function output = result(this, input)
-      output = input;
-      for i = 1:size(this.network)(2)
-        output = (this.network{i}*(Perceptron.addThreshold(output))')';
-      end
+      [V, h] = this.propagate(input);
+      output = V{end};
     end
 
     function learn(this, input, expectedOutput)
-      output = this.propagate(input);
-      this.backpropagate(output, expectedOutput);
+      [V, h] = this.propagate(input);
+      this.backpropagate(V, h, expectedOutput);
+    end
+
+    function learnAll(this, f)
+      patterns = Perceptron.buildBinaryEntries(size(this.network{1})(2)-1);
+      for i = 1:size(patterns)(1)
+        this.learn(patterns(i,:), f(patterns(i,:)));
+        this.costError = [this.costError; this.getError(patterns,f)];
+      end
     end
 
   end
 
   methods (Access = private)
 
-    function output = propagate(this, input)
-      output{1} = input;
+    function [V,h] = propagate(this, input)
+      V{1} = input;
+      h{1} = input;
       for i = 1:size(this.network)(2)
-        output{i+1} = this.activation((this.network{i}*(Perceptron.addThreshold(output{i}))'))';
+        h{i+1} = (this.network{i}*(Perceptron.addThreshold(V{i}))')';
+        V{i+1} = this.activation((this.network{i}*(Perceptron.addThreshold(V{i}))'))';
       end
     end
 
-    function backpropagate(this, output, expectedOutput)
-      deltas = this.calculateDeltas(output, expectedOutput);
+    function backpropagate(this, V, h, expectedOutput)
+      deltas = this.calculateDeltas(V, h, expectedOutput);
       for k = 1:size(this.network)(2)
-        output{k} = Perceptron.addThreshold(output{k});
-        this.network{k} += this.learningRate * deltas{k}' * output{k} ;
+        V{k} = Perceptron.addThreshold(V{k});
+        this.network{k} += this.learningRate * deltas{k}' * V{k};
       end
     end
 
-    function deltas = calculateDeltas(this, output, expectedOutput)
+    function deltas = calculateDeltas(this, V, h, expectedOutput)
       layers = size(this.network)(2);
-      deltas{layers} = this.diffActivation(output{layers+1}).*(expectedOutput - output{layers+1});
+      deltas{layers} = this.diffActivation(h{layers+1}).*(expectedOutput - V{layers+1});
       for k = layers:-1:2
         weights = Perceptron.removeThreshold(this.network{k});
-        deltas{k-1} = this.diffActivation(output{k}).*(deltas{k}*weights);
+        deltas{k-1} = this.diffActivation(h{k}).*(deltas{k}*weights);
       end
+    end
+
+    function plotCost = getError(this,patterns,f)
+      e = 0;
+      for i = 1 : size(patterns)(1)
+        e += (f(patterns(i,:)) - this.result(patterns(i,:)))** 2;
+      end
+      e /= 2* size(patterns)(1);
+      this.costIndex += 1;
+      plotCost = [this.costIndex , e];
     end
 
   end
@@ -76,8 +94,20 @@ classdef Perceptron < handle
       ans = [];
       for i = 1 : size(weights)(1)
           ans = [ans;weights(i,2:end)];
-      endfor
-    endfunction
+      end
+    end
+
+    function retval = buildBinaryEntries (n)
+      X = [];
+      for i = [0:2^n-1]
+         row = [];
+         for j = [0:n-1]
+            row = horzcat(row,bitget(i,n-j));
+         end
+         X = vertcat(X,row);
+      end
+      retval = X;
+    end
 
   end
 
--- helper.m
+++ helper.m
@@ -1,6 +1,12 @@
 function p = helper()
+  a = {}
+  a{1} = rand(4,4);
+  a{2} = rand(4,5);
+  a{3} = rand(1,5);
   f = @(x) tanh(x);
   f_prima = @(x) 1 - tanh(x).^2;
-  p = Perceptron(0.1, [4,1], [3,5], f, f_prima);
-  p.learn([1,1], -1);
+  p = Perceptron(0.1, a, f, f_prima);
+  costerror = [];
 end
+
+f = @(x) xor(x(1),x(2),x(3))
