5d5c61493f0b62e91f2d98e1c6379cf04c810649
nicomarcan <marcantonionicolas@gmail.com>
2018-03-25 23:08:55 -0300

    added configs

---
+++ config2.m
@@ -0,0 +1,14 @@
+mode="incremental";
+g="exp";
+beta = 1;
+eta=0.09;
+momentum=0;
+adaptative=1;
+incrementRate=0.05;
+decrementRate=0.1;
+hidden_layers=1;
+neurons=[40];
+trainRatio = 0.9;
+epsilon = 0.05;;
+cutCondition = 0.00035;
+terrainPath = '../../Downloads/terrain06.data';
---
+++ config3.m
@@ -0,0 +1,14 @@
+mode="incremental";
+g="tanh";
+beta = 0.65;
+eta=0.09;
+momentum=0;
+adaptative=0;
+incrementRate=0.05;
+decrementRate=0.1;
+hidden_layers=1;
+neurons=[40];
+trainRatio = 0.9;
+epsilon = 0.05;;
+cutCondition = 0.00035;
+terrainPath = '../../Downloads/terrain06.data';
