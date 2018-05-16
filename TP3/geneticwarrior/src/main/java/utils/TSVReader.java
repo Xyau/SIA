bdde77d8e955b7a48f0ba672f49a78b22b28009b
Eliseo Parodi Almaraz <eparodialmaraz@gmail.com>
2018-05-16 17:32:31 -0300

    File parser

--- TP3/geneticwarrior/src/main/java/utils/TSVReader.java
+++ TP3/geneticwarrior/src/main/java/utils/TSVReader.java
@@ -1,11 +1,39 @@
 package utils;
 
+import genes.ItemPhenotype;
 import interfaces.Phenotype;
+import main.BonusType;
+import main.ItemType;
 
 import java.io.File;
-import java.util.List;
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+import static java.lang.System.exit;
 
 public class TSVReader {
-    
+
+    public static List<ItemPhenotype> parseFile(String pathfile, ItemType type){
+        List<ItemPhenotype> weapons = new LinkedList<>();
+        File file = new File(pathfile);
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                int id = sc.nextInt();
+                Map<BonusType, Double> bonusMap = new HashMap<>();
+                bonusMap.put(BonusType.STRENGHT, sc.nextDouble());
+                bonusMap.put(BonusType.AGILITY, sc.nextDouble());
+                bonusMap.put(BonusType.WISDOM, sc.nextDouble());
+                bonusMap.put(BonusType.RESISTANCE, sc.nextDouble());
+                bonusMap.put(BonusType.HEALTH, sc.nextDouble());
+                weapons.add(new ItemPhenotype(id + " item", type, bonusMap));
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            exit(0);
+        }
+        return weapons;
+    }
+
 }
